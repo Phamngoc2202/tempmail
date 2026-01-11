d4a1daf12b7714c632a405ec38b62b9005d0afd0
Phamngoc2202 <phamngoc0045@gmail.com>
2026-01-11 13:27:28 +0700

    Fix port for Render

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
+
 
 app.use(express.static('public'));
 
